ff7a861e1cdf29353a930e31c4baa0c1cbb3e8ac
bobbyhadz <opmftw@gmail.com>
2024-03-02 15:22:00 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log('bobbyhadz.com');
+
+// import _ from 'https://cdn.jsdelivr.net/npm/@esm-bundle/lodash@4.17.21/+esm';
+import _ from 'lodash';
+
+console.log(_.uniq([1, 1, 3])); // 👉️ [1, 3]
